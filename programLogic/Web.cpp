f02c76d2ff71d693311cab04c5400c5ac70682d4
schizoid07 <miguelhero700@gmail.com>
2024-06-08 18:21:16 -0400

    problemas en Web.cpp declarando getters

---
+++ programLogic/Web.cpp
@@ -0,0 +1,20 @@
+#include "Web.hpp"
+#include <iostream>
+
+
+//desarrollo de los metodos
+void Web::setUrl( char* urlSetted) {
+    char* url=urlSetted;
+}
+
+void Web::setNombre( char* nombreSetted) {
+     char* nombre=nombreSetted;
+}
+
+char* Web::getUrl() {
+    return url;
+}
+
+char* Web::getNombre() {
+    return nombre;
+}
---
+++ programLogic/Web.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+class Web {
+
+    private:
+         char* url,nombre;
+    
+    public:
+        //getters
+         char* getUrl();
+         char* getNombre();
+        //setters
+        void setUrl( char*);
+        void setNombre( char*);
+};      
+
