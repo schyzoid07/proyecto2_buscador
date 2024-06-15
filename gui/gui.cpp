dce7a8ef9e9ea1834facc66ff65435a000fbddaf
schizoid07 <miguelhero700@gmail.com>
2024-06-15 12:46:27 -0400

    terminados los menues de favoritos, genral de la web y general del programa

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -14,11 +14,12 @@ int Gui::menuPrincipal() {
     std::cout<<"        ¡Bienvenido usuario!"<<std::endl;
     
     while (opcion=-1) {
-        std::cout<<"Por favor, introduzca el numero correspondiente a la opcion que desee ejecutar:"<<std::endl;
+    
     std::cout<<"0. Salir"<<std::endl;
     std::cout<<"1. Navegar a una Pagina Web"<<std::endl;
     std::cout<<"2. Navegar a una Pagina Web de favoritos"<<std::endl;
-    std::cout<<"3. Exportar Paginas Web favoritas a archivo HTML"<<std::endl;
+    std::cout<<"3. Exportar Paginas Web de su lista de Favoritos a un archivo HTML"<<std::endl;
+    std::cout<<"Por favor, introduzca el numero correspondiente a la opcion que desee ejecutar:"<<std::endl;
     std::cin>>opcion;
 
     switch (opcion)
@@ -45,9 +46,26 @@ int Gui::menuPrincipal() {
  
 
 int Gui::menuPaginasWeb() {
-system("cls");
+    system("cls");    
+    std::cout<<"0. Salir"<<std::endl;
+    std::cout<<"1. Navegar hacia adelante"<<std::endl;
+    std::cout<<"2. Navegar hacia atras"<<std::endl;
+    std::cout<<"3. Guardar pagina en la lista de Favoritos"<<std::endl;
+    std::cout<<"4. Eliminar pagina en la lista de Favoritos"<<std::endl;
+    std::cout<<"Por favor, introduzca el numero correspondiente a la opcion que desee ejecutar:"<<std::endl;
+    std::cin>>opcion;
 }
 
+
+
 int Gui::menuPaginasWebFavorito() {
-system("cls");
+    system("cls");    
+    std::cout<<"0. Salir"<<std::endl;
+    std::cout<<"1. Navegar hacia adelante"<<std::endl;
+    std::cout<<"2. Navegar hacia atras"<<std::endl;
+    std::cout<<"3. Eliminar pagina en la lista de Favoritos"<<std::endl;
+    std::cout<<"4. Restaurar Favoritos (los 5 ultimos eliminados)"<<std::endl;
+    std::cout<<"5. Organizar Favoritos"<<std::endl;
+    std::cout<<"Por favor, introduzca el numero correspondiente a la opcion que desee ejecutar:"<<std::endl;
+    std::cin>>opcion;
 }
--- gui/gui.hpp
+++ gui/gui.hpp
@@ -4,13 +4,9 @@
 class Gui
 {
 private:
-//Variable ocn la que el usuario eligira la opcion del menu
+//Variable con la que el usuario eligira la opcion del menu
     int opcion=-1;
-    //0.    Salir
-    //1.    
-    //
-    //
-    //
+
 public:
 //Getters y Setters
 int getOpcion();
